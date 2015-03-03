f96065bd0549fa25b6457766e7468e63d5697405
Juan A. Suarez Romero <jasuarez@igalia.com>
2015-03-03 22:47:10 +0100

    Use UTF-8 enconding

--- init.el
+++ init.el
@@ -12,3 +12,11 @@
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
+
+;; UTF-8 encoding
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
