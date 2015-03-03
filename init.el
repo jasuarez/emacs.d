db5329363f6479edadb07a4229870038d04abc6f
Juan A. Suarez Romero <jasuarez@igalia.com>
2015-03-03 22:45:29 +0100

    Set custom variables

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
-; no splash message
+;; no splash message
 (setq inhibit-splash-screen t)
 
+;; custom variables
+(custom-set-variables
+ '(blink-matching-parent t)
+ '(calendar-week-start-day 1)
+ '(column-number-mode t)
+ '(fill-column 80)
+ '(paren-set-mode (quote sexp))
+ '(safe-local-variable-values (quote ((js-indent-level . 4))))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
