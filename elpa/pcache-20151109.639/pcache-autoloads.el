724f7c08a7b11155f6055df6387f483f9f8ab2c0
Juan A. Suarez Romero <jasuarez@igalia.com>
2015-11-30 08:07:12 +0100

    Update pcache

--- elpa/pcache-20151109.639/pcache-autoloads.el
+++ elpa/pcache-20151109.639/pcache-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("pcache.el") (21752 16827 565731 319000))
+;;;### (autoloads nil nil ("pcache.el") (22107 62838 335475 620000))
 
 ;;;***
 
