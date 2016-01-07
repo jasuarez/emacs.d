384eb95f1d71782f38b4d14a66ed6f4e7e0d2338
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-01-07 09:56:19 +0100

    Update Yaml Mode

--- elpa/yaml-mode-20160101.921/yaml-mode-autoloads.el
+++ elpa/yaml-mode-20160101.921/yaml-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (22132 36611 94770
-;;;;;;  459000))
+;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (22158 10199 91940
+;;;;;;  382000))
 ;;; Generated autoloads from yaml-mode.el
 
 (let ((loads (get 'yaml 'custom-loads))) (if (member '"yaml-mode" loads) nil (put 'yaml 'custom-loads (cons '"yaml-mode" loads))))
