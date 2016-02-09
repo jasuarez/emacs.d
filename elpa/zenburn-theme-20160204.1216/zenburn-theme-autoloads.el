7d18fcf08cc862a90bf8d0ec56dec92d14402d99
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-02-09 09:47:34 +0100

    Update Zenburn theme

--- elpa/zenburn-theme-20160204.1216/zenburn-theme-autoloads.el
+++ elpa/zenburn-theme-20160204.1216/zenburn-theme-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (22181 62118
-;;;;;;  186811 820000))
+;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (22201 42814
+;;;;;;  580565 185000))
 ;;; Generated autoloads from zenburn-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
