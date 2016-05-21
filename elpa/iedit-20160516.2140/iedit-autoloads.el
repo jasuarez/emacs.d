e31c817eca03dfe0cfee75214c2b567359d78ada
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-05-21 19:19:16 +0200

    Update iedit

--- elpa/iedit-20160516.2140/iedit-autoloads.el
+++ elpa/iedit-20160516.2140/iedit-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "iedit" "iedit.el" (22108 19846 455246 298000))
+;;;### (autoloads nil "iedit" "iedit.el" (22336 38874 674430 617000))
 ;;; Generated autoloads from iedit.el
 
 (autoload 'iedit-mode "iedit" "\
@@ -68,8 +68,8 @@ Toggle Iedit mode on current function.
 
 ;;;***
 
-;;;### (autoloads nil "iedit-rect" "iedit-rect.el" (22108 19846 463246
-;;;;;;  327000))
+;;;### (autoloads nil "iedit-rect" "iedit-rect.el" (22336 38874 688430
+;;;;;;  675000))
 ;;; Generated autoloads from iedit-rect.el
 
 (autoload 'iedit-rectangle-mode "iedit-rect" "\
@@ -86,8 +86,8 @@ Commands:
 
 ;;;***
 
-;;;### (autoloads nil nil ("iedit-lib.el" "iedit-pkg.el") (22108
-;;;;;;  19846 496070 195000))
+;;;### (autoloads nil nil ("iedit-lib.el" "iedit-pkg.el") (22336
+;;;;;;  38874 730597 206000))
 
 ;;;***
 
