deae8b15ca3ae943c4b9faabad76b86c59b55080
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-06-17 10:01:19 +0200

    Upgrade Yasnippet

--- elpa/yasnippet-20160612.620/yasnippet-autoloads.el
+++ elpa/yasnippet-20160612.620/yasnippet-autoloads.el
@@ -1,10 +1,10 @@
 ;;; yasnippet-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "yasnippet" "yasnippet.el" (22357 10326 382693
-;;;;;;  152000))
+;;;### (autoloads nil "yasnippet" "yasnippet.el" (22371 44552 455714
+;;;;;;  894000))
 ;;; Generated autoloads from yasnippet.el
 
 (autoload 'yas-minor-mode "yasnippet" "\
@@ -25,7 +25,8 @@ Key bindings:
 
 (defvar yas-global-mode nil "\
 Non-nil if Yas-Global mode is enabled.
-See the command `yas-global-mode' for a description of this minor mode.
+See the `yas-global-mode' command
+for a description of this minor mode.
 Setting this variable directly does not take effect;
 either customize it (see the info node `Easy Customization')
 or call the function `yas-global-mode'.")
@@ -51,8 +52,8 @@ A mode for editing yasnippets
 
 ;;;***
 
-;;;### (autoloads nil nil ("yasnippet-pkg.el") (22357 10326 407737
-;;;;;;  738000))
+;;;### (autoloads nil nil ("yasnippet-pkg.el") (22371 44544 950850
+;;;;;;  909000))
 
 ;;;***
 
