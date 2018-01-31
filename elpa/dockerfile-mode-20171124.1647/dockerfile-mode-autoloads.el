bf36eec15d50cd7991def24001eaf5d61cff4344
Juan A. Suarez Romero <jasuarez@igalia.com>
2018-01-31 20:32:55 +0100

    Update dockerfile mode

--- elpa/dockerfile-mode-20171124.1647/dockerfile-mode-autoloads.el
+++ elpa/dockerfile-mode-20171124.1647/dockerfile-mode-autoloads.el
@@ -3,17 +3,18 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "dockerfile-mode" "dockerfile-mode.el" (22847
-;;;;;;  63210 884315 425000))
+;;;### (autoloads nil "dockerfile-mode" "dockerfile-mode.el" (23154
+;;;;;;  6487 739718 69000))
 ;;; Generated autoloads from dockerfile-mode.el
 
 (autoload 'dockerfile-build-buffer "dockerfile-mode" "\
-Build an image based upon the buffer
+Build an image called IMAGE-NAME based upon the buffer.
+If prefix arg NO-CACHE is set, don't cache the image.
 
-\(fn IMAGE-NAME)" t nil)
+\(fn IMAGE-NAME &optional NO-CACHE)" t nil)
 
 (autoload 'dockerfile-build-no-cache-buffer "dockerfile-mode" "\
-Build an image based upon the buffer without cache
+Build an image called IMAGE-NAME based upon the buffer without cache.
 
 \(fn IMAGE-NAME)" t nil)
 
@@ -23,7 +24,7 @@ A major mode to edit Dockerfiles.
 
 \(fn)" t nil)
 
-(add-to-list 'auto-mode-alist '("Dockerfile.*\\'" . dockerfile-mode))
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 
 ;;;***
 
