9c7a479ff48962279e57d8b706c6e294cac7749f
Juan A. Suarez Romero <jasuarez@igalia.com>
2018-01-31 20:33:55 +0100

    Update gist

--- elpa/gist-20171127.2006/gist-autoloads.el
+++ elpa/gist-20171127.2006/gist-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "gist" "gist.el" (22847 63207 97388 540000))
+;;;### (autoloads nil "gist" "gist.el" (23154 6484 844703 209000))
 ;;; Generated autoloads from gist.el
 
 (autoload 'gist-region "gist" "\
