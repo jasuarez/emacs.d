26d010b56f7a192eb066adfa110312d08151c18e
Juan A. Suarez Romero <jasuarez@igalia.com>
2018-01-31 20:31:04 +0100

    Update clang package

--- elpa/clang-format-20171202.1318/clang-format-autoloads.el
+++ elpa/clang-format-20171202.1318/clang-format-autoloads.el
@@ -3,21 +3,26 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "clang-format" "clang-format.el" (22746 4444
-;;;;;;  946190 624000))
+;;;### (autoloads nil "clang-format" "clang-format.el" (23154 6491
+;;;;;;  430737 10000))
 ;;; Generated autoloads from clang-format.el
 
 (autoload 'clang-format-region "clang-format" "\
 Use clang-format to format the code between START and END according to STYLE.
-If called interactively uses the region or the current statement if there
-is no active region.  If no style is given uses `clang-format-style'.
+If called interactively uses the region or the current statement if there is no
+no active region. If no STYLE is given uses `clang-format-style'. Use
+ASSUME-FILE-NAME to locate a style config file, if no ASSUME-FILE-NAME is given
+uses the function `buffer-file-name'.
 
-\(fn START END &optional STYLE)" t nil)
+\(fn START END &optional STYLE ASSUME-FILE-NAME)" t nil)
 
 (autoload 'clang-format-buffer "clang-format" "\
 Use clang-format to format the current buffer according to STYLE.
+If no STYLE is given uses `clang-format-style'. Use ASSUME-FILE-NAME
+to locate a style config file. If no ASSUME-FILE-NAME is given uses
+the function `buffer-file-name'.
 
-\(fn &optional STYLE)" t nil)
+\(fn &optional STYLE ASSUME-FILE-NAME)" t nil)
 
 (defalias 'clang-format 'clang-format-region)
 
