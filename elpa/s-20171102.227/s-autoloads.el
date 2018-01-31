8c9794b235381bea3a0ca88bc97078114b65820a
Juan A. Suarez Romero <jasuarez@igalia.com>
2018-01-31 20:36:59 +0100

    Update S

--- elpa/s-20171102.227/s-autoloads.el
+++ elpa/s-20171102.227/s-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("s.el") (22847 63202 587475 613000))
+;;;### (autoloads nil nil ("s.el") (23154 6479 131673 895000))
 
 ;;;***
 
