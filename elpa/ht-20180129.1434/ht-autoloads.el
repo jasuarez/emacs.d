ec12145b6269531c4133120cdc0bf60b6c131d72
Juan A. Suarez Romero <jasuarez@igalia.com>
2018-01-31 20:34:51 +0100

    Update HT

--- elpa/ht-20180129.1434/ht-autoloads.el
+++ elpa/ht-20180129.1434/ht-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("ht.el") (22675 10779 294944 974000))
+;;;### (autoloads nil nil ("ht.el") (23154 6483 299695 281000))
 
 ;;;***
 
