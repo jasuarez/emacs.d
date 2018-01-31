9ab76853c5bbf0f3b823f496e19f8f61e082336b
Juan A. Suarez Romero <jasuarez@igalia.com>
2018-01-31 20:34:29 +0100

    Update google C-style

---
+++ elpa/google-c-style-20180130.936/google-c-style-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "google-c-style" "20180130.936" "Google's C/C++ style for c-mode" 'nil :commit "505ba68c74eb97e6966f60907ce893001bedc706" :keywords '("c" "tools"))
