6ae360e9fd12bbf2723f8d6709be43f851f7b05d
Juan A. Suarez Romero <jasuarez@igalia.com>
2018-01-31 20:32:37 +0100

    Update Dash

--- elpa/dash-20180118.743/dash-autoloads.el
+++ elpa/dash-20180118.743/dash-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("dash.el") (22847 63197 407575 622000))
+;;;### (autoloads nil nil ("dash.el") (23154 6488 522722 87000))
 
 ;;;***
 
