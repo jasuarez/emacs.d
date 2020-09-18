dd7ca6f3c972ce9e9c05ab03233220fd97550b17
Juan A. Suarez Romero <jasuarez@igalia.com>
2020-09-18 13:59:38 +0200

    Store customs in a different file
    Custom package make changes in the init.el file by default, which is
    something we don't really want.
    
    So let's define a separate file where Custom can record its changes.

--- init.el
+++ init.el
@@ -1,2 +1,7 @@
 ;; No splash message
 (setq inhibit-splash-screen t)
+
+;; Store Customs in different place
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
