c847985e95b6babccef2ea196ace657179891239
Juan A. Suarez Romero <jasuarez@igalia.com>
2020-09-18 14:00:49 +0200

    Install use-package (if not installed)
    It's actually a macro, but anyway.
    
    It simplifies a lot how to handle and configure the different packages
    to install.

--- init.el
+++ init.el
@@ -5,3 +5,17 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
+
+;; Install use-package if not already installed
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
