74a500fd999e2ef7386422d576d80bf0585381d2
Juan A. Suarez Romero <jasuarez@igalia.com>
2020-09-18 14:05:42 +0200

    Do not create backup files
    By default, after editing a file and saving it, a copy of the previous
    content is stored as <filename>~.
    
    We don't want to pollute with those files.

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 ;; No splash message
 (setq inhibit-splash-screen t)
 
+;; No backup files
+(setq make-backup-files nil)
+
 ;; Store Customs in different place
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
