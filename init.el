edba399369a79a06160564fd76cc5a66493c4c9d
Juan A. Suarez Romero <jasuarez@igalia.com>
2020-09-21 19:48:12 +0200

    Install TRAMP Docker
    TRAMP integration for docker containers.
    But instead of using docker, we will use podman, which is the one coming
    in Fedora.

--- init.el
+++ init.el
@@ -27,6 +27,13 @@
   (require 'use-package-ensure)
   (setq use-package-always-ensure t))
 
+;; TRAMP integration for docker containers
+;; Use podman instead of docker
+
+(use-package docker-tramp
+  :custom
+  (docker-tramp-docker-executable "podman"))
+
 ;; Major mode for editing Docker's Dockerfiles
 (use-package dockerfile-mode)
 
