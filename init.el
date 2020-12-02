6a7985f013b3b4693b4816acfd97bb03fb869496
Juan A. Suarez Romero <jasuarez@igalia.com>
2020-12-02 13:16:45 +0100

    Install and configure Org package
    Outline-based notes management and organizer.

--- init.el
+++ init.el
@@ -81,6 +81,25 @@
 ;; Major mode for the Meson build system files
 (use-package meson-mode)
 
+;; Outline-based notes management and organizer
+(use-package org
+  :config
+  (setq org-directory "~/Misc/orgfiles")
+  (setq org-agenda-files (list org-directory))
+  (setq org-refile-use-outline-path 'file)
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 1)))
+  (setq org-capture-templates
+	'(("t" "Todo [Inbox]" entry
+	   (file "~/Misc/orgfiles/inbox.org")
+	   "* TODO %i%?")))
+  (setq org-clock-persist t)
+  (setq org-clock-in-resume t)
+  (setq org-clock-persist-query-resume nil)
+  (setq org-clock-out-remove-zero-time-clocks t)
+  (org-clock-persistence-insinuate)
+  :bind
+  (("C-c c" . org-capture)))
+
 ;; Org Twiki and Foswiki export
 (use-package ox-twiki)
 
