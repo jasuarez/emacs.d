b9c9dbe4dbe8089e856f19cb9c06b15b7839aace
Juan A. Suarez Romero <jasuarez@igalia.com>
2024-05-24 06:04:57 +0200

    Melpa: use https to fetch packages

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
 
 ;; Install use-package if not already installed
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (unless package-archive-contents
